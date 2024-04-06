75cd6700756ce9b034e6ecd4cc3679889370c0fc
Alexei <robinbobin35@gmail.com>
2024-04-07 00:58:56 +0500

    lesson assignment

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-def name_fun(*num):
-    for i in num:
-        print(i)
-
-
-name_fun(1, 2, 3, 4)
+list_01 = [1, 2, 3, 5, 8, 15, 23, 38]
+print(list_01)
+list_01 = [i for i in list_01 if i % 2 == 0]
+print(list_01)
+list_01 = [(i, i * i) for i in list_01]
+print(list_01)
