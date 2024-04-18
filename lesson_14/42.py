f0e265f87da5825b8c205130ceedfc4e52be40dc
Alexei <robinbobin35@gmail.com>
2024-04-18 15:40:46 +0400

    Add lesson 14 - 40

---
+++ lesson_14/42.py
@@ -0,0 +1,12 @@
+# Google Colab
+# Задача 42
+# Дан файл california_housing_train.csv.
+# Найти максимальное значение переменной "households" в зоне минимального значения
+# переменной min_population и сохраните это значение в переменную max_households_in_min_population.
+# Используйте модуль pandas.
+
+import pandas
+df = pandas.read_csv('california_housing_train.csv')
+min_population = df['population'].min()
+max_households_in_min_population = df[df['population'] == min_population]['households'].max()
+# print(max_households_in_min_population)
